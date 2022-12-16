ff36026ad427c708c0256cb65e58fa5e8d14602d
krzysiek.krowka <krzysztof.krowka@onet.eu>
2022-12-16 20:59:23 +0100

    feat: upload full project

---
+++ clientData/clientData/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clientData
+{
+    public class Client
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set {
+                if (!char.IsUpper(value[0]))
+                    throw new System.Exception("Name must start with uppercase letter");
+                else
+                    name = value; 
+            }
+                
+        }
+        
+        private string surname;
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (!char.IsUpper(value[0]))
+                    throw new System.Exception("Surname must start with uppercase letter");
+                else
+                    surname = value;
+            }
+
+        }
+        
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (!(value >= 100 && value <= 210))
+                    throw new System.Exception("Height must be within 100-210 range");
+                else
+                    height = value;
+            }
+
+        }
+    }
+}
---
+++ clientData/clientData/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace clientData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> clients = new List<Client>();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine($"\nAdd client nr {clients.Count+1} (name,surname,height)");
+                    var client=new Client();
+                    client.Name = Console.ReadLine();
+                    client.Surname = Console.ReadLine();
+                    client.Height = int.Parse(Console.ReadLine());
+                    clients.Add(client);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("\n---------------------------------------");
+                    for (int i = 0; i < clients.Count; i++)
+                    {
+                        Console.WriteLine($"\nClient nr {i+1}");
+                        Console.WriteLine(clients[i].Name);
+                        Console.WriteLine(clients[i].Surname);
+                        Console.WriteLine(clients[i].Height);                  
+                    }
+                    Console.WriteLine("---------------------------------------");
+                };
+            }
+        }
+    }
+}
